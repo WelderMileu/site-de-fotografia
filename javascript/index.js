0b0f2af32efc562b7b97c176e3d30edbdd6232b4
Welder Mileu <welderfelix21@gmail.com>
2019-10-09 16:31:05 -0300

    Em desenvolvimento

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,21 @@
 (function(){
 	const $ = (e) => document.querySelector(e);
 
+	// toggle menu
+
 	$(".bars").addEventListener("click", () => {
 		$(".navbar-nav").classList.toggle("toggle");
 	});
+
+	// Mudar cor do menu quanto o top chegar a 10px
+	window.addEventListener("scroll", () => {
+		if (window.scrollTop >= 10) {
+			$(".nav").style.backgroudColor = "black";
+			console.log($(".content").scrollTop);
+		}else{
+			$(".nav").style.backgroudColor = "blue";
+			console.log($(".content").scrollTop);
+		}
+	})
 
 })();
